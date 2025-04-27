f8b446ca96341c7bd04afa2d7cba4c44c74a7009
Liron Bakshi <lironbakshi@hotmail.com>
2025-04-27 15:25:16 +0300

    Initial Dockerization, DB set-up and Post Routes of the web service

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM python:3.11-slim
+
+# Set working directory
+WORKDIR /app
+
+# Copy requirements first (for better caching)
+COPY requirements.txt .
+
+# Install dependencies
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copy all app code
+COPY . .
+
+# Run server
+CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000", "--reload"]
---
+++ app/routes/POSTRequests.py
@@ -0,0 +1,43 @@
+from fastapi import APIRouter, HTTPException
+from pydantic import BaseModel
+import redis.asyncio as redis
+
+router = APIRouter()
+
+r = redis.Redis(host='redis', port=6379, decode_responses=True)
+
+class Song(BaseModel):
+    title: str
+    youtube_link: str
+
+@router.post("/{username}")
+async def create_user(username: str):
+    user_key = f"user:{username}"
+
+    if await r.exists(user_key):
+        raise HTTPException(status_code=409, detail="User already exists")
+
+    await r.hset(user_key, "initialized", "true")
+    return {"message": "User created successfully"}
+
+@router.post("/{username}/songs")
+async def add_song(username: str, song: Song):
+    user_key = f"user:{username}"
+    if not await r.exists(user_key):
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    song.youtube_link = song.youtube_link.lower() # For consistency
+
+    song_title_key = f"song:{username}:{song.title}"
+    song_link_key = f"songlink:{username}:{song.youtube_link}"
+
+    if await r.exists(song_title_key):
+        raise HTTPException(status_code=409, detail="Song's title already exists under this user")
+
+    if await r.exists(song_link_key):
+        raise HTTPException(status_code=409, detail="Song's link already exists under this user")
+
+    await r.set(song_title_key, song.youtube_link) 
+    await r.set(song_link_key, song.title)
+
+    return {"message": "Song added successfully"}
