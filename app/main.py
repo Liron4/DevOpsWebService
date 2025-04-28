fefb0ddd12f8c5307d4decf9f8373cf7396ef0b0
CoralShahaff <coshahaff@gmail.com>
2025-04-28 22:19:59 +0300

    Added GET request and added redis aof for git tracking

--- app/main.py
+++ app/main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
 import uvicorn
 from app.routes.POSTRequests import router as post_router
+from app.routes.GETRequests import router as get_router
 
 app = FastAPI(title="Playlist Server API")
 
 # POST routes
 app.include_router(post_router, prefix="/POST")
+
+# GET routes
+app.include_router(get_router, prefix="/GET")
 
 if __name__ == "__main__":
     uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
