4e260450da406e97836e5909384ac57bc56bbae6
Saar Salhov <saarsalhov@gmail.com>
2025-05-26 21:36:06 +0300

    Comments

--- app/main.py
+++ app/main.py
@@ -7,13 +7,13 @@ from app.routes.DELETERequests import router as delete_router
 app = FastAPI(title="Playlist Server API")
 
 # POST routes
-app.include_router(post_router, prefix="/POST")
+app.include_router(post_router, prefix="/POST") # bad name for router, router shouldnt have the name of the HTTP method
 
 # GET routes
-app.include_router(get_router, prefix="/GET")
+app.include_router(get_router, prefix="/GET") # bad name for router, router shouldnt have the name of the HTTP method
 
 # DELETE routes
-app.include_router(delete_router, prefix="/DELETE")
+app.include_router(delete_router, prefix="/DELETE") # bad name for router, router shouldnt have the name of the HTTP method
 
 if __name__ == "__main__":
     uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
